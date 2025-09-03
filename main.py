b48d44efb90cbfe53c5de6e115e69775c821a66b
paulo-campos-57 <paulo.m.campos6601@gmail.com>
2025-09-03 12:08:55 -0300

    add: initialize project

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class User(BaseModel):
+    user: str
+
+@app.post("/auth/me")
+async def auth_me(user: User):
+    return {"user": user.user, "ping": "pong"}
