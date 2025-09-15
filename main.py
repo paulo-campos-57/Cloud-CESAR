320401c978360d00a122caabfccd36ff7467531c
paulo-campos-57 <paulo.m.campos6601@gmail.com>
2025-09-15 12:00:27 -0300

    feat: create route for /

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "From FastAPI"}
+
+
 @app.post("/auth/me")
 async def auth_me(username: str):
     return {"user": username, "ping": "pong"}
